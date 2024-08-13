46fa00bb76b94fefd1218f7f6ed749cd7aaaf861
stanabaz <anas.abazid@student.hs-anhalt.de>
2024-08-13 15:43:23 +0200

    This is a very simple example about JavaFX

---
+++ FirstProject/src/module-info.java
@@ -0,0 +1,16 @@
+module javafxsimplepiechart.firstproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens javafxsimplepiechart.firstproject to javafx.fxml;
+    exports javafxsimplepiechart.firstproject;
+}
